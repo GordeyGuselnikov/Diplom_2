001736c2583ad9cc11622841a22f34d41a01a565
Gordey Guselnikov <g.guselnikov@bi.zone>
2022-09-07 11:39:04 +0700

    fix constant and public static final

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -1,18 +1,18 @@
 package constant;
 
-public interface Constant {
+public class Constant {
 
-    String BASE_URI = "https://stellarburgers.nomoreparties.site";
-    boolean EXPECTED_RESULT_TRUE = true;
-    boolean EXPECTED_RESULT_FALSE = false;
-    String EMPTY_ACCESS_TOKEN = "";
-    String[] EMPTY_INGREDIENTS = {};
-    String[] INGREDIENTS = {"61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6c", "61c0c5a71d1f82001bdaaa72"};
-    String[] INVALID_INGREDIENTS_HASH = {"000", "111", "222"};
-    String ALREADY_EXISTS = "User already exists";
-    String REQUIRED_FIELDS = "Email, password and name are required fields";
-    String DEFAULT_EMAIL = "test-data@yandex.ru";
-    String EMPTY_PASSWORD = "";
-    String DEFAULT_PASSWORD = "password";
-    String INCORRECT_FIELDS = "email or password are incorrect";
+    public static final String BASE_URI = "https://stellarburgers.nomoreparties.site";
+    public static final boolean EXPECTED_RESULT_TRUE = true;
+    public static final boolean EXPECTED_RESULT_FALSE = false;
+    public static final String EMPTY_ACCESS_TOKEN = "";
+    public static final String[] EMPTY_INGREDIENTS = {};
+    public static final String[] INGREDIENTS = {"61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6c", "61c0c5a71d1f82001bdaaa72"};
+    public static final String[] INVALID_INGREDIENTS_HASH = {"000", "111", "222"};
+    public static final String ALREADY_EXISTS = "User already exists";
+    public static final String REQUIRED_FIELDS = "Email, password and name are required fields";
+    public static final String DEFAULT_EMAIL = "test-data@yandex.ru";
+    public static final String EMPTY_PASSWORD = "";
+    public static final String DEFAULT_PASSWORD = "password";
+    public static final String INCORRECT_FIELDS = "email or password are incorrect";
 }
